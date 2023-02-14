5cb810f8c4815135ed0a1812db0042d315228b6d
willianchu <willianchu@hotmail.com>
2023-02-13 19:04:34 -0800

    comparing with match

--- mainGuess.rs
+++ mainGuess.rs
@@ -11,10 +11,14 @@ fn main() {
     let mut guess = String::new();
 
     io::stdin() 
-        .read_line(&mut guess) 
+        .read_line(&mut guess)
         .expect("Failed to read line"); 
 
     println!("You guessed: {guess}");
 
-    println!("The secret number is: {secret_number}")
+    match guess.cmp(&secret_number) { 
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
